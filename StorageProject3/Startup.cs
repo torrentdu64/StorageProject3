d27086a730ce6aa5a6e47031752a51776bfaeed6
admin <torrentdu64@gmail.com>
2020-06-18 10:16:02 +1200

    Add project files.

---
+++ StorageProject3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StorageProject3.Startup))]
+namespace StorageProject3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
