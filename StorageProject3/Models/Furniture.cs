b2e21096eb77d7304681ee14a2078a7c58dd3c38
admin <torrentdu64@gmail.com>
2020-06-27 16:47:24 +1200

    before add-migation create a class Furniture with DepoId

---
+++ StorageProject3/Models/Furniture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StorageProject3.Models
+{
+    public class Furniture
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public Depot Depot { get; set; }
+        public int DepotId { get; set; }
+        
+    }
+}
